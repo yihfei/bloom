4a80ed29235c1454bdb07b09a639e851d3147cff
yihfei <lim.yih.fei@gmail.com>
2025-06-19 01:06:08 +0200

    feat(auth): implement google auth for prod and local

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -3,9 +3,13 @@
 import { signIn, signOut } from "@/auth";
 
 export const login = async () => {
-  await signIn("github", { redirectTo: "/" });
+    await signIn();
 };
 
 export const logout = async () => {
-  await signOut({ redirectTo: "/" });
+    await signOut({
+        redirectTo: "/",
+    });
 };
+
+
--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,12 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google"
+
 import prisma from "@/lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [GitHub],
+  providers: [GitHub, Google], 
   adapter: PrismaAdapter(prisma),
   
 });
